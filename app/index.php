bb678dbe02f1ab0104123d526fa71be49ecee464
orangebal <40687295+orangebal@users.noreply.github.com>
2018-12-31 12:06:24 +0900

    Update index.php

--- app/index.php
+++ app/index.php
@@ -8,10 +8,10 @@
   <?php echo '<p>Hello World</p>'; ?>
   <?php echo '<p>Hello World</p>'; ?>
   <?php echo '<p>Hello World</p>'; ?>
-  <?php echo '<p>Hello World</p>'; ?>
-  <?php echo '<p>Hello World</p>'; ?>
-  <?php echo '<p>Hello World</p>'; ?>
-  <?php echo '<p>Hello World</p>'; ?>
+  <?php echo '<p>welcome World</p>'; ?>
+  <?php echo '<p>welcome World</p>'; ?>
+  <?php echo '<p>welcome World</p>'; ?>
+  <?php echo '<p>welcome World</p>'; ?>
   <?php echo '<p>Hello World</p>'; ?>
  </body>
 </html>
