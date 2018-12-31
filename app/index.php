a5438d9aa169eab1ce050acfc7978947230fcde0
orangebal <40687295+orangebal@users.noreply.github.com>
2018-12-31 13:55:40 +0900

    Update index.php

--- app/index.php
+++ app/index.php
@@ -3,15 +3,15 @@
   <title>PHP 테스트</title>
  </head>
  <body>
-  <?php echo '<p>Hello World</p>'; ?> 
-  <?php echo '<p>Hello World</p>'; ?>
-  <?php echo '<p>Hello World</p>'; ?>
-  <?php echo '<p>Hello World</p>'; ?>
-  <?php echo '<p>Hello World</p>'; ?>
-  <?php echo '<p>welcome World</p>'; ?>
-  <?php echo '<p>welcome World</p>'; ?>
-  <?php echo '<p>welcome World</p>'; ?>
-  <?php echo '<p>welcome World</p>'; ?>
-  <?php echo '<p>Hello World</p>'; ?>
+  <?php echo '<p>welcome to world</p>'; ?> 
+  <?php echo '<p>welcome to world</p>'; ?>
+  <?php echo '<p>welcome to world</p>'; ?>
+  <?php echo '<p>welcome to world</p>'; ?>
+  <?php echo '<p>welcome to world</p>'; ?>
+  <?php echo '<p>welcome to world</p>'; ?>
+  <?php echo '<p>welcome to world</p>'; ?>
+  <?php echo '<p>welcome to world</p>'; ?>
+  <?php echo '<p>welcome to world</p>'; ?>
+  <?php echo '<p>welcome to world</p>'; ?>
  </body>
 </html>
